f1ca2c70af82160b5fd883dd39789e8047026b65
harithakh <hkhulangamuwa@gmail.com>
2023-08-19 10:13:38 +0530

    debug data passing error in partials

--- index.js
+++ index.js
@@ -14,30 +14,35 @@ app.set("view engine", "ejs");
 // Define the path to the views directory
 app.set("views", path.join(__dirname, "views"));
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
- 
-app.get('/', (req, res) => { 
-  res.render('index');
+app.use(express.static(path.join(__dirname, "public")));
+
+const phoneObjects = [
+  { model: "Aquos Sense7", img: "../img/aquos-sense7.jpg" },
+  { model: "Iphone 13", img: "../img/iphone-13.jpg" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { phones: phoneObjects });
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get('/signup', (req, res) => {
-  res.render('signup');
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-app.get('/reviews', (req, res) => {
-  res.render('reviews');
+app.get("/reviews", (req, res) => {
+  res.render("reviews");
 });
 
-app.get('/search', (req, res) => {
-  res.render('search');
+app.get("/search", (req, res) => {
+  res.render("search");
 });
 
-app.get('/account', (req, res) => {
-  res.render('account');
+app.get("/account", (req, res) => {
+  res.render("account");
 });
 
 app.listen(port, () => {
