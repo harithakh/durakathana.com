96203b471eaaa065adea12749bb5bafb60c94be6
harithakh <hkhulangamuwa@gmail.com>
2023-11-17 16:35:03 +0530

    add env variables to dbconnection.js

--- public/js/dbconnection.js
+++ public/js/dbconnection.js
@@ -1,11 +1,15 @@
 // import mysql from 'mysql2';
 //database server must have started
 import mysql from 'mysql2/promise'; 
+import { config } from 'dotenv';
+
+// Load environment variables from a .env file in the same directory
+config();
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'scsci',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: 'slmobi',
   connectionLimit: 10
 });
